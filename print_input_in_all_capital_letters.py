3dfa33b767c33b778f51c58126a4feda952966d0
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:32:29 +0800

    user_input_fullname

---
+++ print_input_in_all_capital_letters.py
@@ -0,0 +1,6 @@
+# Objective: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+# 1. Ask the user to input their full name.
+
+fname = input('Enter your full name: ')
+
