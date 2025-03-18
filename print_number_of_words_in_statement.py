378da1e1a25f54058a89e615511e91f755f95b50
ycz <yczaubhreydy@gmail.com>
2025-03-19 03:08:17 +0800

    added_len_and_split_functions

--- print_number_of_words_in_statement.py
+++ print_number_of_words_in_statement.py
@@ -2,3 +2,7 @@
 
 # 1. Ask the user the input a complete statement.
 comp_statement = input('Please enter a complete statement: ')
+
+# 2.  Count the number of words in the statement. 
+# 3. Print the number of words in the statement. 
+print(len(comp_statement.split()))
