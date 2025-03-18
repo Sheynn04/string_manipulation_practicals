89d73e27709d94b086fca375dfb9248b9504189b
ycz <yczaubhreydy@gmail.com>
2025-03-19 03:00:36 +0800

    user_input_statement

---
+++ print_number_of_words_in_statement.py
@@ -0,0 +1,4 @@
+# Objective: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+# 1. Ask the user the input a complete statement.
+comp_statement = input('Please enter a complete statement: ')
