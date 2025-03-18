468eaa42e05c522e5ba1c5edaf7ca00993929e3f
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:40:26 +0800

    added_lower_method

--- print_input_in_all_lowercase.py
+++ print_input_in_all_lowercase.py
@@ -3,3 +3,6 @@
 # 1. Ask the user to input their full name.
 fname = input('Input your full name: ')
 
+# 2. Convert all the characters of the input into lowercase.
+# 3. Print the converted input.
+print(fname.lower())
