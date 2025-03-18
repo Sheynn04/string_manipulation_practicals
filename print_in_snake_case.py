152737730f2ed64a20ce037a3f405ef6aa23a4bc
ycz <yczaubhreydy@gmail.com>
2025-03-19 03:31:37 +0800

    user_input_and_fixed_pseudocode

---
+++ print_in_snake_case.py
@@ -0,0 +1,4 @@
+# Objective: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+# 1. Ask the user to input their fullname.
+fname = input("Input your name in improper casing: ")
--- print_number_of_words_in_statement.py
+++ print_number_of_words_in_statement.py
@@ -3,6 +3,7 @@
 # 1. Ask the user the input a complete statement.
 comp_statement = input('Please enter a complete statement: ')
 
-# 2.  Count the number of words in the statement. 
-# 3. Print the number of words in the statement. 
+# 2. Split the statement into a list of words.
+# 3. Count the number of words. 
+# 4. Print the number of words in the statement. 
 print(len(comp_statement.split()))
