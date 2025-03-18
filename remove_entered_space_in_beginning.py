fa105bdba9ac4549a31d270eb38b3f979d9db92b
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:08:12 +0800

    name_input_trial

--- remove_entered_space_in_beginning.py
+++ remove_entered_space_in_beginning.py
@@ -1,1 +1,6 @@
-print('hello')
+# Objective: Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# 1. Ask the user to input their full name with several space characters in the beginning 
+
+fname = input('Enter your several spaces then your full name: ')
+print(fname)
