6d745b8c3c39b5439219a025b9eb7c73c96138b1
ycz <yczaubhreydy@gmail.com>
2025-03-19 03:35:07 +0800

    added_lower_and_replace_function

--- print_in_snake_case.py
+++ print_in_snake_case.py
@@ -2,3 +2,8 @@
 
 # 1. Ask the user to input their fullname.
 fname = input("Input your name in improper casing: ")
+
+# 2. Convert the characters of the input into lowercase.
+# 3. Replace the spaces with an underscore.
+# 4. Print the fullname in snake case format.
+print(fname.lower().replace(" ","_"))
