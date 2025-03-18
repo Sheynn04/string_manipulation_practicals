94e023461463968b229b8c49fbc5344e8f204f84
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:15:45 +0800

    removed_unnecessary_pseudo

--- remove_entered_space_in_beginning.py
+++ remove_entered_space_in_beginning.py
@@ -1,4 +1,4 @@
-# Objective: Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+# Objective: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # 1. Ask the user to input their full name with several space characters in the beginning 
 
