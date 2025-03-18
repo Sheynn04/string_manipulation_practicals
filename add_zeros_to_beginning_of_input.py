58ab3879ea1a0ed87691b43d2a197cf582b219c2
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:24:10 +0800

    added_zfill_method

--- add_zeros_to_beginning_of_input.py
+++ add_zeros_to_beginning_of_input.py
@@ -3,3 +3,8 @@
 # 1. Ask the user to input a number from 0-1000.
 
 number = input('Enter a number from 0 - 1000: ')
+
+# 2. Add zeros to the beginning of the input to turn it into 6 digit format.
+# 3. Print the 6 digit format the previous input.
+
+print(number.zfill(6))
