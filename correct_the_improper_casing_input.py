7e6c21f59ce10bd74769b8162522dc1992199ff2
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:50:54 +0800

    added_title_method

--- correct_the_improper_casing_input.py
+++ correct_the_improper_casing_input.py
@@ -2,3 +2,7 @@
 
 # 1. Ask the user the input their full name in improper casing. 
 fname = input('Input your name in improper casing: ')
+
+# 2. Convert the input into proper casing. 
+# 3. Print the proper cased full name. 
+print(fname.title())
