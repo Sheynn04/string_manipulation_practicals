50b681baa8e70ef99c3d40fc4399201ca206312f
ycz <yczaubhreydy@gmail.com>
2025-03-19 03:27:51 +0800

    added_title_and_replace_method

--- print_in_pascal_case.py
+++ print_in_pascal_case.py
@@ -3,3 +3,7 @@
 # 1. Ask the user the input their fullname in incorrect casing. 
 fname = input('Input your fullname in improper casing: ')
 
+# 2. Convert the input into proper casing.
+# 3. Remove the spaces in between the names.
+# 4. Print the pascal format of their fullname.
+print(fname.title().replace(" ",""))
