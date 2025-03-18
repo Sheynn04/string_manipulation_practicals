6dd8149195322a9d4806be8ca0b3bb80660c87a0
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:46:53 +0800

    user_input_fullname

---
+++ correct_the_improper_casing_input.py
@@ -0,0 +1,4 @@
+# Objective: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+# 1. Ask the user the input their full name in improper casing. 
+fname = input('Input your name in improper casing: ')
