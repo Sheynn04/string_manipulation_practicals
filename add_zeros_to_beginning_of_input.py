7967daa1fc081de84d0b0f3613f5631ade229e2b
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:41:43 +0800

    fixed_petpeeves

--- add_zeros_to_beginning_of_input.py
+++ add_zeros_to_beginning_of_input.py
@@ -1,10 +1,8 @@
 # Objective: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 # 1. Ask the user to input a number from 0-1000.
-
 number = input('Enter a number from 0 - 1000: ')
 
 # 2. Add zeros to the beginning of the input to turn it into 6 digit format.
 # 3. Print the 6 digit format the previous input.
-
 print(number.zfill(6))
--- print_input_in_all_capital_letters.py
+++ print_input_in_all_capital_letters.py
@@ -1,11 +1,9 @@
 # Objective: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 # 1. Ask the user to input their full name.
-
 fname = input('Enter your full name: ')
 
 # 2. Convert the input into all capital letters.
-# 3. Print the capital lettered input
-
+# 3. Print the capital lettered input.
 print(fname.upper())
 
