8b59a243154ba473b0d39a72bdfff299d0e33206
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:14:03 +0800

    added_strip_function

--- remove_entered_space_in_beginning.py
+++ remove_entered_space_in_beginning.py
@@ -3,4 +3,8 @@
 # 1. Ask the user to input their full name with several space characters in the beginning 
 
 fname = input('Enter your several spaces then your full name: ')
-print(fname)
+
+# 2. Remove the space from the left side of the full name.
+# 3. Print the full name
+
+print(fname.lstrip())
