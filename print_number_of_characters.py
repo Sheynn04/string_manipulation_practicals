a11f8d75c267265eb6e84a902fb6dd59bf5ee5c0
ycz <yczaubhreydy@gmail.com>
2025-03-19 03:18:16 +0800

    added_len_and_replace_method

--- print_number_of_characters.py
+++ print_number_of_characters.py
@@ -3,3 +3,6 @@
 # 1. Ask the user to input their fullname.
 fname = input("Input your fullname: ")
 
+# 2. Count the characters in the fullname.
+# 3. Print the length of the characters.
+print(len(fname.replace(" ", "")))
