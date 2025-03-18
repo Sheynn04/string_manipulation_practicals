47d65c568ccd9e58ff3b405841656cde896b517a
ycz <yczaubhreydy@gmail.com>
2025-03-19 02:55:02 +0800

    user_input_fname

---
+++ print_input_in_reversed_casing.py
@@ -0,0 +1,4 @@
+# Objective: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+# 1. Ask the user to input their full name in incorrect casing.
+fname = input("Input your name in incorrect casing: ")
--- remove_entered_space_in_beginning.py
+++ remove_entered_space_in_beginning.py
@@ -1,6 +1,6 @@
 # Objective: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
-# 1. Ask the user to input their full name with several space characters in the beginning
+# 1. Ask the user to input their full name with several space characters in the beginning.
 fname = input('Enter your several spaces then your full name: ')
 
 # 2. Remove the space from the left side of the full name.
